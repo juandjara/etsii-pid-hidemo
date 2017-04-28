c654cc3c7a05e0fea98446e239d05fc2caec1fd0
ernestdtv <ernestodtv@gmail.com>
2017-04-28 14:20:12 +0200

    CoordinatesScript

--- Assets/Scripts/CoordinatesScript.cs
+++ Assets/Scripts/CoordinatesScript.cs
@@ -1,16 +1,37 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//
+//public class Coordinates : MonoBehaviour {
+//
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+//}
+
+// Draws a line in the scene view going through a point 200 pixels
+// from the lower-left corner of the screen
 using UnityEngine;
+using System.Collections;
 
-public class Coordinates : MonoBehaviour {
+public class ExampleClass : MonoBehaviour
+{
+	Camera camera;
 
-	// Use this for initialization
-	void Start () {
-		
+	void Start()
+	{
+		camera = GetComponent<Camera>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void Update()
+	{
+		Ray ray = camera.ScreenPointToRay(new Vector3(200, 200, 0));
+		Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
 	}
 }
