4c06529a479f5eece4dab4a9f07fd3f1417437af
juandjara <juanorigami@gmail.com>
2017-05-12 14:13:15 +0200

    calculos de crear cubos

--- Assets/Scripts/ObjetoOnClickScript.cs
+++ Assets/Scripts/ObjetoOnClickScript.cs
@@ -4,39 +4,31 @@ using UnityEngine;
 
 public class ObjetoOnClickScript : MonoBehaviour {
 
-	Ray ray;
 	RaycastHit hit;
 	public GameObject prefab;
 
-
-	void Start () {
-
-
-
-
-	}
-
-
 	void Update () {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-		ray=Camera.main.ScreenPointToRay(Input.mousePosition);
+		if(Physics.Raycast(ray, out hit)) {
+			if(Input.GetMouseButtonDown(0)) {
+				Vector3 objectCenter = hit.transform.position;
+				Vector3 fromCenterToClickPoint = hit.point - objectCenter;
 
-		if(Physics.Raycast(ray,out hit))
-		{
-
-			if(Input.GetMouseButtonDown(0))
-			{
-
-				float x= Mathf.RoundToInt (hit.point.x); 
-				float y= Mathf.RoundToInt(hit.point.y);
-				float z= Mathf.RoundToInt(hit.point.z);
+				float x = Mathf.RoundToInt (hit.point.x); 
+				float y = Mathf.RoundToInt (hit.point.y);
+				float z = Mathf.RoundToInt (hit.point.z);
 				
-				GameObject obj=Instantiate(prefab,new Vector3(x+0.5f,y+0.5f,z+0.5f), Quaternion.identity) as GameObject;
+				float xDirection = Mathf.Sign(fromCenterToClickPoint.x);
+				float yDirection = Mathf.Sign(fromCenterToClickPoint.y);
+				float zDirection = Mathf.Sign(fromCenterToClickPoint.z);
 
+				Vector3 direction = new Vector3(xDirection*0.5f, yDirection*0.5f, zDirection*0.5f);
+
+				Vector3 position = new Vector3(x+0.5f,y+0.5f,z+0.5f);
+
+				GameObject obj=Instantiate(prefab, position + direction, Quaternion.identity) as GameObject;
 			}
-
 		}
-
-
-}
+	}
 }
