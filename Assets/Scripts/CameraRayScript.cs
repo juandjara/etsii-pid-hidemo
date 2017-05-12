3b8004f66ee4eaca25e8ec91857d91f192d7e6c9
juandjara <juanorigami@gmail.com>
2017-05-12 13:23:52 +0200

    script de raycast

---
+++ Assets/Scripts/CameraRayScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRayScript : MonoBehaviour {
+
+	private Camera cam;
+
+	// Use this for initialization
+	void Start () {
+		cam = GetComponent<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(0)) {
+			RaycastHit hit;
+			Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+			if(Physics.Raycast(ray, out hit)) {
+				Debug.Log("HIT "+hit.point.ToString());
+			}
+		}
+	}
+}
